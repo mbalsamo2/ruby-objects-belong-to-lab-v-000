f38ae65f9614acae44b19ba8619af44621ff532a
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-06 17:39:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   attr_accessor :name
 
   def initialize(name)
-    
+
     @name = name
 
   end
--- lib/song.rb
+++ lib/song.rb
@@ -6,3 +6,12 @@ class Song
   end
 
 end
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
